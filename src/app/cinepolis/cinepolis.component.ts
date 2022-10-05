d54efedefeb39e5c8081f2ccaea4b253baaadb89
bere1206 <iselacortes87@gmail.com>
2022-10-04 19:07:20 -0500

    segundo commit

--- src/app/cinepolis/cinepolis.component.ts
+++ src/app/cinepolis/cinepolis.component.ts
@@ -7,26 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CinepolisComponent implements OnInit {
   nombre:string='';
-  cantComp:number=0;
+  comp:number=0;
+  cantComp:string='';
   tarjeta:any='';
   cantBol:any='';
   precioBol:any=12;
-  total:any=0;
-  total2:number=0;
+  total:any='';
 
   constructor() { }
 
   totalPago():void{
+    this.comp=parseInt(this.cantComp)*7;
+    if(this.cantBol<=this.comp){
+
     if (this.cantBol>5) {
       this.total=(parseInt(this.cantBol)*parseInt(this.precioBol))*0.85;
     }else if (this.cantBol>=3 && this.cantBol<=5){
       this.total=(parseInt(this.cantBol)*parseInt(this.precioBol))*0.9;
     }else if (this.cantBol<=2){
       this.total=parseInt(this.cantBol)*parseInt(this.precioBol);
-    }else if(this.tarjeta.onclick('true')){
-      this.total=parseInt(this.total)*.9;
+    }
+    if(this.tarjeta=='si'){
+      this.total=parseInt(this.total)*0.9;
     }
   }
+}
 
   ngOnInit(): void {
   }
